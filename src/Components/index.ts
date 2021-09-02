be6e1f36a08ad090d2cbd2781d6e87107724bc13
irlgabriel <radu.gabriel131@gmail.com>
2021-09-02 13:02:54 +0300

    indexed some components;

--- src/Components/index.ts
+++ src/Components/index.ts
@@ -1,1 +1,6 @@
 export { Router } from "./Router";
+export { Cell } from "./Cell";
+export { Legend } from "./Legend";
+export { Map } from "./Map";
+export { Navbar } from "./Navbar";
+export { Overlay } from "./Overlay";
